a996b02aaba3cb61088892849db3ec8c638c8406
Alfie Torres <alfielytorres@gmail.com>
2020-08-02 17:21:27 +1000

    added first

---
+++ introductory/week 1/light-bulb.py
@@ -0,0 +1,54 @@
+import math
+
+
+# # A 
+# answer = math.pow(math.e,math.log(10,10))
+# print(answer)
+
+
+# # B 
+# answer = math.sin((1/2)*math.pi)
+# print(answer)
+
+
+# # D
+# answer = math.sqrt(math.pow(5,3+1))
+# print(answer)
+
+
+# E
+answer = math.factorial(5)/10
+print(answer)
+
+# E 
+
+
+# H 
+
+
+# I 
+
+# L 
+
+
+
+# M 
+
+
+# N 
+
+# O 
+
+# P 
+
+
+# R 
+
+
+# S 
+
+
+# T 
+
+
+# W 
---
+++ introductory/week 1/math-module.py
@@ -0,0 +1,66 @@
+import math 
+
+# sqrt 
+num = 10
+answer = math.sqrt(num)
+print("\nSqrt of {} is {}\n".format(num, answer))
+
+# ceil
+num = 20.12313
+answer = math.ceil(num)
+print("Ceil of {} is {}\n".format(num, answer ))
+
+# floor
+num = 20.12313
+answer = math.floor(num)
+print("Floor of {} is {}\n".format(num, answer ))
+
+# factorial
+num = 5
+answer = math.factorial(num)
+print("Factorial of {} is {}\n".format(num, answer ))
+
+# cos 
+num = 5
+answer = math.cos(num)
+print("Cosine of {} is {}\n".format(num, answer ))
+
+
+# sin
+num = 5
+answer = math.sin(num)
+print("Sine of {} is {}\n".format(num, answer ))
+
+
+# exp 
+num = 4
+eulers = math.e
+answer = math.exp(num)
+print("{} raised to {} is {}\n".format(eulers, num, answer ))
+
+
+# log
+argument = 16
+base = 4
+answer = math.log(argument,base)
+print("log of {} base {} is {}\n".format(argument,base,answer ))
+
+# pow
+base = 4
+exponent = 2
+answer = math.pow(base,exponent)
+print("{} raise to {} is {}\n".format(base,exponent,answer ))
+
+# pi 
+
+answer = math.pi
+print("pi {}\n".format(answer))
+
+
+# e 
+answer = math.e
+print("euler {}\n".format(answer))
+
+# inf
+answer = -math.inf
+print("inf {}\n".format(answer))
